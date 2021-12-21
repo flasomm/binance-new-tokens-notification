717a76ff07eb07d4a59d554baed61900498eda25
Fabrice Sommavilla <flasomm@gmail.com>
2021-12-21 23:55:03 +0100

    chore: send email and format code

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const fs = require('fs');
-const {crawlUrl} = require('./lib/scraper');
+const { crawlUrl } = require('./lib/scraper');
+const { getListing } = require('./lib/get-listing');
+const { sendEmail } = require('./lib/email');
 
-const BINANCE_NEW_CRYPTO_LISTING_URL = "https://www.binance.com/en/support/announcement/c-48";
+const BINANCE_NEW_CRYPTO_LISTING_URL = 'https://www.binance.com/en/support/announcement/c-48';
 
 (async () => {
-    try {
-        const scrapedList = await crawlUrl(BINANCE_NEW_CRYPTO_LISTING_URL);
+  try {
+    const scrapedList = await crawlUrl(BINANCE_NEW_CRYPTO_LISTING_URL);
+    const newsList = getListing(scrapedList);
+    await sendEmail(newsList);
 
-        const lastReleaseDate = fs.readFileSync('release.txt', 'utf8');
-        let currentReleaseDate = new Date().getTime();
+    console.info(`Found new cryptos`, newsList);
 
-        const newsList = scrapedList.reduce((acc, {title, releaseDate}) => {
-            console.log(new Date(releaseDate), new Date(parseInt(lastReleaseDate)));
-            console.log(new Date(releaseDate) > new Date(parseInt(lastReleaseDate)));
-            if (!lastReleaseDate || new Date(releaseDate) > new Date(parseInt(lastReleaseDate))) {
-                console.log("title", title);
-                acc.push(title);
-                currentReleaseDate = releaseDate;
-            }
-            return acc;
-        }, []);
-
-        //fs.writeFileSync('release.txt', currentReleaseDate.toString());
-
-        console.info(`Found new cryptos`, newsList);
-
-    } catch (err) {
-        console.error(err);
-    }
+  } catch (err) {
+    console.error(err);
+  }
 })();
